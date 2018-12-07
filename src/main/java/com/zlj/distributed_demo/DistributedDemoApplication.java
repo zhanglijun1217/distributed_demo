6b3a0b86af45ab99659952ac874948b96456bece
张利君 <kuake@youzan.com>
2018-12-08 00:53:12 +0800

    分布式demo初次提交吗

---
+++ src/main/java/com/zlj/distributed_demo/DistributedDemoApplication.java
@@ -0,0 +1,15 @@
+package com.zlj.distributed_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 基于spring boot的一些分布式解决方案的demo
+ */
+@SpringBootApplication
+public class DistributedDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedDemoApplication.class, args);
+	}
+}
